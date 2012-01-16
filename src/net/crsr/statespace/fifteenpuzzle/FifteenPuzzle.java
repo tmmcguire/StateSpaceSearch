ff879f9dc1202d8a4ddc532617f2d188d7676bc2
Tommy M. McGuire <mcguire@crsr.net>
2012-01-15 19:22:54 -0600

    Added two heuristic methods and harder puzzle

--- src/net/crsr/statespace/fifteenpuzzle/FifteenPuzzle.java
+++ src/net/crsr/statespace/fifteenpuzzle/FifteenPuzzle.java
@@ -8,35 +8,44 @@ import java.util.Set;
 
 import net.crsr.statespace.Search;
 
-public class FifteenPuzzle {
+public class FifteenPuzzle
+{
 
-	public static void main(String[] args)
-	{
-		System.out.println("Uninformed, breadth first");
-		Queue<FState> fifo = new ArrayDeque<FState>();
-		runSearch(fifo, new HashSet<FState>());
-		
-		System.out.println("Heuristic search");
-		Queue<FState> priority = new PriorityQueue<FState>();
-		runSearch(priority, new HashSet<FState>());
-	}
+  public static void main(String[] args)
+  {
+    System.out.println("Uninformed, breadth first");
+    Queue<FState> fifo = new ArrayDeque<FState>();
+    runSearch(fifo, new HashSet<FState>(), FState.oneOut());
 
-	private static void runSearch(Queue<FState> queue, Set<FState> seen)
-	{
-		Search<FState> search = new Search<FState>(queue, seen, FState.oneOut());
-		FState end = search.findGoal();
-		if (end != null)
-		{
-			System.out.format("Solution:\n");
-			for (FState st = end; st != null; st = st.previous)
-			{
-				System.out.format("%s\n", st);
-			}
-		}
-		else
-		{
-			System.out.format("No solution found\n");
-		}
-		System.out.format("%d states examined\n", search.statesExamined());
-	}
+    System.out.println("Heuristic search");
+    Queue<FState> priority = new PriorityQueue<FState>();
+    runSearch(priority, new HashSet<FState>(), FState.oneOut());
+
+    System.out.println("Heuristic search: manhattan distance");
+    priority = new PriorityQueue<FState>();
+    runSearch(priority, new HashSet<FState>(), FState.harder());
+
+    System.out.println("Heuristic search: out of position");
+    priority = new PriorityQueue<FState>(100, new FState.OutOfPosition());
+    runSearch(priority, new HashSet<FState>(), FState.harder());
+  }
+
+  private static void runSearch(Queue<FState> queue, Set<FState> seen, FState initial)
+  {
+    Search<FState> search = new Search<FState>(queue, seen, initial);
+    FState end = search.findGoal();
+    if (end != null)
+    {
+      System.out.format("Solution:\n");
+      for (FState st = end; st != null; st = st.previous)
+      {
+        System.out.format("%s\n", st);
+      }
+    }
+    else
+    {
+      System.out.format("No solution found\n");
+    }
+    System.out.format("%d states examined\n", search.statesExamined());
+  }
 }
